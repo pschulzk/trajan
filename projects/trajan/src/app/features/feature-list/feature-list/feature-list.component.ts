4d2e776266008bd55c1e907aeb43a2a111bdca41
Philip Schulz Klingauf <philip@schulz-klingauf.de>
2019-11-10 03:25:13 +0100

    feat(feature-list): wip - implementing search

--- projects/trajan/src/app/features/feature-list/feature-list/feature-list.component.ts
+++ projects/trajan/src/app/features/feature-list/feature-list/feature-list.component.ts
@@ -14,28 +14,30 @@ import {
   TagResponse
 } from '../../../shared/models/server-models';
 import { Receipt } from '../../../shared/models/receipt.model';
-import { Observable } from 'rxjs';
+import { Observable, combineLatest, Subject, BehaviorSubject } from 'rxjs';
 import { Router } from '@angular/router';
-import { filter, map, tap, startWith } from 'rxjs/operators';
+import {
+  filter,
+  map,
+  tap,
+  startWith,
+  takeUntil,
+  debounceTime
+} from 'rxjs/operators';
 import { ContentDataService } from '../../../shared/providers/content-data/content-data.service';
-import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
+import { FormGroup, FormControl } from '@angular/forms';
 import {
   MatAutocomplete,
   MatAutocompleteSelectedEvent,
   MatChipInputEvent
 } from '@angular/material';
 
-interface FeatureListFormGroup {
-  searchTerm: FormControl | string;
-  tagsInclude: FormControl | any;
-}
-
 @Component({
   selector: 'anms-feature-list',
   templateUrl: './feature-list.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class FeatureListComponent implements OnInit {
+export class FeatureListComponent implements OnDestroy, OnInit {
   routeAnimationsElements = ROUTE_ANIMATIONS_ELEMENTS;
 
   receiptIngredientTags$: Observable<TagResponse[]>;
@@ -44,33 +46,30 @@ export class FeatureListComponent implements OnInit {
   receiptCategories$: Observable<TagResponse[]>;
   receipts$: Observable<NodeResponse<Receipt>[]>;
 
-  tagsSelected: TagResponse[] = [];
   visible = true;
   selectable = true;
   addOnBlur = true;
   separatorKeysCodes: number[] = [ENTER, COMMA];
-  fruitCtrl = new FormControl();
-  filteredFruits: Observable<string[]>;
-  fruits: string[] = ['Lemon'];
-  allFruits: string[] = ['Apple', 'Lemon', 'Lime', 'Orange', 'Strawberry'];
+  inputTagIncludeControl = new FormControl();
+  allTags: string[] = [];
+  filteredTags$: Observable<string[]>;
+  tagsSelected: string[] = [];
 
   formGroup: FormGroup;
-  @ViewChild('fruitInput', { static: false }) fruitInput: ElementRef<
+  @ViewChild('inputSearchTerm', { static: false }) inputSearchTerm: ElementRef<
     HTMLInputElement
   >;
+  @ViewChild('inputTagsInclude', { static: false })
+  inputTagsInclude: ElementRef<HTMLInputElement>;
   @ViewChild('auto', { static: false }) matAutocomplete: MatAutocomplete;
 
-  formGroupInitControl: FeatureListFormGroup = {
-    searchTerm: new FormControl(''),
-    tagsInclude: new FormControl([
-      this.tagsSelected,
-      FeatureListComponent.validateRequired
-    ])
-  };
-
-  get formGroupValues$(): Observable<FeatureListFormGroup> {
-    return this.formGroup.valueChanges;
+  set searchTerm(v: string) {
+    this._searchTerm$.next(v);
   }
+  get searchTerm$(): Observable<string> {
+    return this._searchTerm$.asObservable();
+  }
+  private _searchTerm$ = new BehaviorSubject<string>(null);
 
   receiptCategoryColors = [
     '#ac8391',
@@ -82,46 +81,54 @@ export class FeatureListComponent implements OnInit {
     '#32acd9'
   ];
 
-  static validateRequired(c: FormControl) {
-    if (c.value.length === 0) {
-      return { required: true };
-    } else {
-      return null;
-    }
-  }
+  private destroyed = new Subject<void>();
 
-  constructor(
-    private router: Router,
-    private content: ContentDataService,
-    private formBuilder: FormBuilder
-  ) {}
+  constructor(private router: Router, private content: ContentDataService) {}
 
   ngOnInit() {
     this.receiptIngredientTags$ = this.content.getTagsOfTagFamily(
       this.content.tagFamilyIngedrientsUuid
     );
-    this.receiptIngredientTagsFiltered$ = this.receiptIngredientTags$.pipe(
-      map(items =>
-        items.filter(item => {
-          return new RegExp(
-            `(${this.formGroup.get('tagsInclude').value})`,
-            'gi'
-          ).test(item.name);
-        })
+
+    this.receiptIngredientTags$
+      .pipe(
+        map(tags =>
+          tags
+            .map(tag => tag.name)
+            .filter(tag => !this.tagsSelected.includes(tag))
+        ),
+        takeUntil(this.destroyed)
       )
-    );
+      .subscribe(tags => (this.allTags = tags));
+
+    // this.receiptIngredientTagsFiltered$ = combineLatest([
+    //   this.receiptIngredientTags$,
+    //   this.searchTerm$,
+    // ]).pipe(
+    //   map(([tags, searchTerm]) =>
+    //     tags.filter(tag => {
+    //       return new RegExp(
+    //         `(${searchTerm})`,
+    //         'gi'
+    //       ).test(tag.name);
+    //     })
+    //   )
+    // );
 
     this.receiptCategories$ = this.content.getReceiptCategories();
     this.receipts$ = this.content.getReceiptsAll();
 
-    this.formGroup = this.formBuilder.group(this.formGroupInitControl);
-
-    this.filteredFruits = this.fruitCtrl.valueChanges.pipe(
+    this.filteredTags$ = this.inputTagIncludeControl.valueChanges.pipe(
       startWith(null),
-      map((fruit: string | null) =>
-        fruit ? this._filter(fruit) : this.allFruits.slice()
+      map((tag: string | null) =>
+        tag ? this._filter(tag) : this.allTags.slice()
       )
     );
+  }
+
+  ngOnDestroy(): void {
+    this.destroyed.next();
+    this.destroyed.complete();
   }
 
   getNodeFieldBinary(nodeUuid: string, thumbnail: string): string {
@@ -135,13 +142,30 @@ export class FeatureListComponent implements OnInit {
   getReceiptsOfReceiptCategory(
     receiptCategoryName: string
   ): Observable<NodeResponse<Receipt>[]> {
-    return this.receipts$.pipe(
-      map((receipts: NodeResponse<Receipt>[]) => {
-        return receipts.filter(receipt =>
-          receipt && receipt.tags.find(tag => tag.name === receiptCategoryName)
-            ? true
-            : false
-        );
+    return combineLatest([this.receipts$, this.searchTerm$]).pipe(
+      map(([receipts, searchTerm]: [NodeResponse<Receipt>[], string]) => {
+        return receipts.filter(receipt => {
+          const matchesReceiptCategory =
+            receipt &&
+            receipt.tags.some(tag => {
+              return tag.name === receiptCategoryName;
+            });
+          const matchesSearchTerm =
+            receipt && ['', null, undefined].includes(searchTerm)
+              ? true
+              : new RegExp(searchTerm, 'gi').test(receipt.fields.content);
+          const matchesIngredientTag =
+            receipt && this.tagsSelected.length === 0
+              ? true
+              : this.tagsSelected.some(tagSelected => {
+                  return receipt.tags
+                    .map(tag => tag.name)
+                    .includes(tagSelected);
+                });
+          return (
+            matchesReceiptCategory && matchesSearchTerm && matchesIngredientTag
+          );
+        });
       })
     );
   }
@@ -152,40 +176,36 @@ export class FeatureListComponent implements OnInit {
     if (!this.matAutocomplete.isOpen) {
       const input = event.input;
       const value = event.value;
-
       // Add our fruit
       if ((value || '').trim()) {
-        this.fruits.push(value.trim());
+        this.tagsSelected.push(value.trim());
       }
-
       // Reset the input value
       if (input) {
         input.value = '';
       }
-
-      this.fruitCtrl.setValue(null);
+      this.inputTagIncludeControl.setValue(null);
     }
   }
 
   remove(fruit: string): void {
-    const index = this.fruits.indexOf(fruit);
-
+    const index = this.tagsSelected.indexOf(fruit);
     if (index >= 0) {
-      this.fruits.splice(index, 1);
+      this.tagsSelected.splice(index, 1);
     }
   }
 
   selected(event: MatAutocompleteSelectedEvent): void {
-    this.fruits.push(event.option.viewValue);
-    this.fruitInput.nativeElement.value = '';
-    this.fruitCtrl.setValue(null);
+    this.tagsSelected.push(event.option.viewValue);
+    this.inputTagsInclude.nativeElement.value = '';
+    this.inputTagIncludeControl.setValue(null);
   }
 
   private _filter(value: string): string[] {
     const filterValue = value.toLowerCase();
 
-    return this.allFruits.filter(
-      fruit => fruit.toLowerCase().indexOf(filterValue) === 0
+    return this.allTags.filter(
+      tag => tag.toLowerCase().indexOf(filterValue) === 0
     );
   }
 }
