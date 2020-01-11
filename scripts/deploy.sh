b503bd39f3c3a6833cc6d6ac2c07f711225fea74
Philip Schulz Klingauf <philip@schulz-klingauf.de>
2020-01-11 16:11:12 +0100

    fix: implemented deploy script setting permissions

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -13,18 +13,27 @@ then
 	echo "${SCRIPT_PREFIX} Delete old dist files archive."
 	rm dist.tar.gz
 fi
+
 echo "${SCRIPT_PREFIX} Compress new local dist files."
 tar -C dist/trajan/ -czf dist.tar.gz .
+
 if ssh -i $PATH_LOCAL_SSHCERT $REMOTE_LOGIN stat "/home/${REMOTE_USER_NAME}/dist.tar.gz" \> /dev/null 2\>\&1
 then
 	echo "${SCRIPT_PREFIX} Delete old dist files archive."
 	ssh -i $PATH_LOCAL_SSHCERT $REMOTE_LOGIN sudo rm "/home/${REMOTE_USER_NAME}/dist.tar.gz"
 fi
+
 echo "${SCRIPT_PREFIX} Upload new dist files."
 scp -i $PATH_LOCAL_SSHCERT dist.tar.gz "${REMOTE_LOGIN}:/home/${REMOTE_USER_NAME}/"
+
 echo "${SCRIPT_PREFIX} Delete old dist files."
 ssh -i $PATH_LOCAL_SSHCERT $REMOTE_LOGIN sudo rm -fr "${REMOTE_PATH_HTML}/*"
+
 echo "${SCRIPT_PREFIX} Extract new dist files."
 ssh -i $PATH_LOCAL_SSHCERT $REMOTE_LOGIN sudo tar xzf "/home/${REMOTE_USER_NAME}/dist.tar.gz" -C "${REMOTE_PATH_HTML}/"
+
+echo "${SCRIPT_PREFIX} Set permissions of new dist files."
+ssh -i $PATH_LOCAL_SSHCERT $REMOTE_LOGIN sudo chgrp -R nginx "${REMOTE_PATH_HTML}/*"
+
 echo "${SCRIPT_PREFIX} SUCCESS"
 exit 0
